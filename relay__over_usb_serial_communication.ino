5025eb5464ec09dcb4f2412e3670c747aeb69c15
Christiaan Dotze <32298617+StiffMedal30@users.noreply.github.com>
2021-10-25 09:40:35 +0200

    removed comments

--- relay__over_usb_serial_communication.ino
+++ relay__over_usb_serial_communication.ino
@@ -2,13 +2,11 @@
 String command;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(relayPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if(Serial.available()){
     command = Serial.readStringUntil('\n');
     command.trim();
